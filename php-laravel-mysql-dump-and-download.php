2e963f1a134262dd73105dc32629eae144720aa7
Luca Cilfone <info@fareilweb.com>
2022-04-02 22:46:15 +0200

    Create php-laravel-mysql-dump-and-download.php

---
+++ php-laravel-mysql-dump-and-download.php
@@ -0,0 +1,37 @@
+<?php 
+
+Route::get('dbdump', function() {
+    // return die('Comment me to use');
+
+    $DB_HOST = env('DB_HOST');
+    $DB_USERNAME = env('DB_USERNAME');
+    $DB_PASSWORD = env('DB_PASSWORD');
+    $DB_DATABASE = env('DB_DATABASE');
+
+    $dumpFileName = date('YmdHis') . '.sql';
+    $dumpFilePath = realpath(app_path('../../dbdumps')) . "/{$dumpFileName}";
+
+    // Make the dump
+    $dumpCommand = "mysqldump --no-tablespaces --user={$DB_USERNAME} --password={$DB_PASSWORD} --host={$DB_HOST} {$DB_DATABASE} --result-file={$dumpFilePath} 2>&1";
+    set_time_limit(0);
+    exec($dumpCommand, $out);
+
+    // Compress the dump into a zip archive
+    $zip = new ZipArchive();
+    $zip->open("{$dumpFilePath}.zip", ZipArchive::CREATE);
+    $zip->addFile($dumpFilePath, $dumpFileName);
+    $zip->close();
+
+    // Delete the dump
+    unlink($dumpFilePath);
+
+    if (file_exists("{$dumpFilePath}.zip")) {
+        return response()->download("{$dumpFilePath}.zip", "{$dumpFileName}.zip");
+    } else {
+        return 'Error: <br>' . array_reduce($out, function($carry, $item) {
+            if (empty($carry)) { $carry =  ''; }
+            return $carry .= $item . '<br>';
+        });
+    }
+});
+
